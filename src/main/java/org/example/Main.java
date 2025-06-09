1d17bfc2df52eafc400c81e52fda961d46f760f9
vikicec <wi24b071@technikum-wien.at>
2025-06-09 21:15:29 +0200

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome!!!!!!! HOOO");
+        System.out.printf("game end notification");
 
     }
 }
