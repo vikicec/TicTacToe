e1ef75ec116951e61eba6c1003433de0940367b4
wi24b161 <wi24b161@technikum-wien.at>
2025-06-12 11:07:56 +0200

    main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,9 +2,10 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-
-
-
+            Player player1 = new Player('X');
+            Player player2 = new Player('O');
+            TicTacToe game = new TicTacToe(player1,player2);
+            game.start();
 
     }
 }
